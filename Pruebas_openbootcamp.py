9f5550e6a721161c6d42cb4ee59029e1d29a5679
Benjamin Burga <80055813+benjaminburga@users.noreply.github.com>
2023-06-23 00:09:14 -0500

    Update Pruebas_openbootcamp.py

--- Pruebas_openbootcamp.py
+++ Pruebas_openbootcamp.py
@@ -1,9 +1,13 @@
 
-#Escribe un programa en la consola de python que pida al usuario su peso (en kg) y estatura (en metros), calcule el índice de masa corporal y lo almacene en una variable, e imprima por pantalla la frase Tu índice de masa corporal es donde es el índice de masa corporal calculado redondeado con dos decimales. Tienes que subir capturas de pantalla en una carpeta comprimida zip.
+#Escribe un programa que sea capaz de mostrar los números del 1 al 100 en orden inverso.
 
 
-peso=(float(input("Ingresa tu peso en KILOS ")))
-estatura=(float(input("Escribe tu estaura en METROS ")))
-IMC= peso / estatura ** 2
+print("This is the numbers")
 
-print(f"Your IMC is  {IMC:.2f} ")
+max_value= 100
+min_value = 0 
+i = max_value
+while  i >=  min_value:
+    print(i)
+        
+    i -= 1
